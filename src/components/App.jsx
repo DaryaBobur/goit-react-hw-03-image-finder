6048cdb0c1084fd16ba1ea84ab447194c9ee821d
Darya <daryabobur@gmail.com>
2022-09-18 21:07:08 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import { Component } from 'react';
-import GetSearchImages from './GetSearchImages';
+// import GetSearchImages from './GetSearchImages';
 import Searchbar from './Searchbar/Searchbar';
 import ImageGallery from './ImageGallery/ImageGallery';
 
