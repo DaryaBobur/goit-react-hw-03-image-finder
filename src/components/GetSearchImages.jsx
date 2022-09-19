aa459aa7d53f3199e9ceafed450b239147fc9cdd
Darya <daryabobur@gmail.com>
2022-09-19 22:00:39 +0300

    Update GetSearchImages.jsx

--- src/components/GetSearchImages.jsx
+++ src/components/GetSearchImages.jsx
@@ -4,16 +4,15 @@ const BASE_URL = 'https://pixabay.com/api/';
 const KEY = '28839601-0c610efa4f554b6dcd03095ae';
 const OPTIONS = 'image_type=photo&orientation=horizontal&per_page=12'
 
- const GetSearchImages = async (name) => {
-  try {
-    const response = await axios.get(`${BASE_URL}?q=${name}&page=2&key=${KEY}&${OPTIONS}`);
-    return response;
-  } catch (error) {
-    console.log(error.message);
-  }
+ const GetSearchImages = async ({ searchQuery = '', currentPage = 1}) => {
+  return await axios.get(`${BASE_URL}?q=${searchQuery}&page=${currentPage}&key=${KEY}&${OPTIONS}`)
+  .then(response => response.data.hits);
+ 
+ 
   
   }
 
-  // GetSearchImages('flower');
-
-  export default GetSearchImages;
+  const API = {
+    GetSearchImages
+  }
+  export default API;
