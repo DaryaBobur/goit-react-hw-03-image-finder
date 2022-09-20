0d674bb88200110dbe25195a49307e4b356eb7ac
Darya <daryabobur@gmail.com>
2022-09-20 21:29:46 +0300

    Create Modal.jsx

---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,13 @@
+
+const Modal = ({items}) => {
+return (
+<div className="overlay">
+  <div className="modal">
+    {items.map(({id, tags, largeImageURL}) => (<img key={id} src={largeImageURL} alt={tags} />))}
+    
+  </div>
+</div>
+)
+}
+
+export default Modal;
