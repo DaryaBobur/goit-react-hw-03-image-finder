7a3a9b368b1db2cd28602d7795b10d77fb17fdb8
Darya <daryabobur@gmail.com>
2022-09-20 21:29:41 +0300

    Update ImageGallery.jsx

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,11 +1,14 @@
 import ImageGalleryItem from "components/ImageGalleryItem/ImageGalleryItem";
-
+import Modal from "components/Modal/Modal";
 const ImageGallery =({items}) => {
     return (
     <ul className="gallery">
     <ImageGalleryItem items={items}/>
+    <Modal items={items}/>
     </ul>
+
     )
+    
 }
 
 export default ImageGallery;
