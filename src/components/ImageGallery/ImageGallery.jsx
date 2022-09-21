e2b87bfc6c3f755eaf43186457cc524453fa9920
Darya <daryabobur@gmail.com>
2022-09-21 17:20:52 +0300

    Update ImageGallery.jsx

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,14 +1,18 @@
+import PropTypes from 'prop-types';
 import ImageGalleryItem from "components/ImageGalleryItem/ImageGalleryItem";
-import Modal from "components/Modal/Modal";
+
 const ImageGallery =({items}) => {
     return (
-    <ul className="gallery">
-    <ImageGalleryItem items={items}/>
-    <Modal items={items}/>
-    </ul>
+        <ul className="gallery">
+            <ImageGalleryItem 
+              items={items}
+            />
+        </ul>
+    )  
+}
 
-    )
-    
+ImageGallery.propTypes = {
+    items: PropTypes.array.isRequired,
 }
 
 export default ImageGallery;
