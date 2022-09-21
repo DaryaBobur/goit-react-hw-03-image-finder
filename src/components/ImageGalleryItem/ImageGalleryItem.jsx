96cba30c6e09b103240bafcef7fdd88f108de585
Darya <daryabobur@gmail.com>
2022-09-21 17:20:55 +0300

    Update ImageGalleryItem.jsx

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,12 +1,20 @@
+import PropTypes from 'prop-types';
 
 const ImageGalleryItem = ( {items} ) => {
-return (
+  return (
     items.map(({id, webformatURL, tags}) => (
-        <li key={id}>
+      <li key={id}>
         <img src={webformatURL} alt={tags} />
       </li>
-      ))
-)
+    ))
+  )
+}
+
+ImageGalleryItem.propTypes = {
+  items: PropTypes.array,
+  id: PropTypes.number,
+  webformatURL: PropTypes.string,
+  tags: PropTypes.string,
 }
 
 export default ImageGalleryItem; 
