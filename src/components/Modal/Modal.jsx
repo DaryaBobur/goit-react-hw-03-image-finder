f92cbe1809dab221f461f8e565789866ab214b66
Darya <daryabobur@gmail.com>
2022-09-21 17:20:56 +0300

    Update Modal.jsx

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,13 +1,13 @@
+// import PropTypes from 'prop-types';
 
 const Modal = ({items}) => {
-return (
-<div className="overlay">
-  <div className="modal">
-    {items.map(({id, tags, largeImageURL}) => (<img key={id} src={largeImageURL} alt={tags} />))}
-    
-  </div>
-</div>
-)
+  return (
+    <div className="overlay">
+      <div className="modal">
+        {items.map(({id, tags, largeImageURL}) => (<img key={id} src={largeImageURL} alt={tags} />))}  
+      </div>
+    </div>
+  )
 }
 
 export default Modal;
