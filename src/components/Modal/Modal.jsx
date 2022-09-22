f9f391067f3d051716876db52114242468d8e45b
Darya <daryabobur@gmail.com>
2022-09-22 17:17:56 +0300

    Update Modal.jsx

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,13 +1,35 @@
-// import PropTypes from 'prop-types';
+import { Component } from "react";
+import { createPortal } from "react-dom"
+import { Overlay, ModalWindow } from "./ModalStyled";
 
-const Modal = ({items}) => {
-  return (
-    <div className="overlay">
-      <div className="modal">
-        {items.map(({id, tags, largeImageURL}) => (<img key={id} src={largeImageURL} alt={tags} />))}  
-      </div>
-    </div>
-  )
+const modalRoot = document.getElementById('modal-root');
+
+class Modal extends Component {
+
+  componentDidMount() {
+    document.addEventListener("keydown", this.closeModal);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener("keydown", this.closeModal)
+  }
+
+  closeModal = ({target, currentTarget, code}) => {
+    if (target === currentTarget || code === "Escape") {
+      this.props.onClose();
+    }
+  }
+
+  render() {
+    return createPortal(
+      <Overlay className="overlay" onClick={this.closeModal}>
+        <ModalWindow className="modal">
+          {this.props.children}
+        </ModalWindow>
+      </Overlay>,
+      modalRoot
+    )
+  }
 }
 
 export default Modal;
